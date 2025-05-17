3d67e1fe53166d85cef2cfd09709fa3f459b9b93
22aryja <dagerson19@mail.ru>
2025-05-17 20:20:23 +0500

    added microservices

---
+++ season-service/src/test/java/org/example/seasonservice/SeasonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seasonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
